45ac9ec5e6b59ca7f1b01f1504b021c527132703
Samuel Moelius <sam@moeli.us>
2026-05-12 05:34:08 -0400

    Search ancestor directories for unset.txt

--- tests/integration.rs
+++ tests/integration.rs
@@ -18,3 +18,21 @@ fn integration() {
         .assert()
         .success();
 }
+
+#[test]
+fn integration_from_child_directory() {
+    Command::new("cargo")
+        .args([
+            "test",
+            "--config",
+            &format!(
+                "target.'cfg(all())'.runner = '{}'",
+                env!("CARGO_BIN_EXE_unset"),
+            ),
+        ])
+        .env("CARGO_TERM_COLOR", "always")
+        .env("VARIABLE_TO_LEAVE_UNCHANGED", "unchanged")
+        .current_dir("fixture/src")
+        .assert()
+        .success();
+}
